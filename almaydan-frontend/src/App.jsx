31c6c4c63672021d60272816c49e48dbd3d5fc60
janaalturki <163104999+janaalturki@users.noreply.github.com>
2026-01-30 01:30:30 +0300

    Navbar

--- almaydan-frontend/src/App.jsx
+++ almaydan-frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <h1>الميداان</h1>
+    
+    <><Navbar />
+    
+    </>
+  
+
   )
 }
 
---
+++ almaydan-frontend/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import './Navbar.css';
+
+const Navbar = () =>{
+    return(
+        <nav className="navbar">
+
+            <div className="nav-logo">
+                <span style={{color: '#00ff88', fontWeight: 'bold', fontSize: '1.5rem'}}>M</span>
+            </div> 
+
+            <ul className="nav-links">
+                <li>جدول المباريات </li>
+                <li><span className="live-dot">●</span>الآن</li>
+                <li>الساحة </li>
+                <li>المعسكر</li>
+                <li>اسال حميدان</li>
+                <li>حول</li>
+
+            </ul>
+
+            <div className="nav-left-icons">
+                <span className="search-icon">🔍</span>
+            
+            </div>
+            
+
+
+        </nav>
+    )
+
+}
+export default Navbar;
