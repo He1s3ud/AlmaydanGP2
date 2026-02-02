6f44cb7f5f1e3d938a39663a48d22b48fb19f7e4
He1s3ud <131931996+He1s3ud@users.noreply.github.com>
2026-02-02 14:16:04 +0300

    uytdmt

--- almaydan-frontend/src/App.jsx
+++ almaydan-frontend/src/App.jsx
@@ -1,18 +1,8 @@
-import './App.css'
-
-import Navbar from './components/Navbar'
-import Floating from './components/Floating/floating'
-import Hero from './sections/Hero/Hero'
-import Showcase from './sections/showcase/showcase'
-
 function App() {
   return (
-    <>
-      <Navbar />
-      <Floating />
-      <Hero />
-      <Showcase />
-    </>
+    <div style={{ color: 'white', padding: '50px' }}>
+      HELLO TEST
+    </div>
   )
 }
 
