1486ac29882c6c8ce4139526c8919d1cfa1d786b
He1s3ud <131931996+He1s3ud@users.noreply.github.com>
2026-02-02 14:29:42 +0300

    gvdfv

--- almaydan-frontend/src/App.jsx
+++ almaydan-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Floating from './components/Floating/floating'
+import Hero from './sections/Hero/Hero'
 
 function App() {
   return (
     <>
       <Navbar />
       <Floating />
+      <Hero />
     </>
   )
 }
