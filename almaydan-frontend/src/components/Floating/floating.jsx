ea96494aea3cf9ac02d02b00988f58279b9629f6
He1s3ud <131931996+He1s3ud@users.noreply.github.com>
2026-02-02 14:24:52 +0300

    mtfmtf

--- almaydan-frontend/src/components/Floating/floating.jsx
+++ almaydan-frontend/src/components/Floating/floating.jsx
@@ -1,11 +1,26 @@
-import './Floating.css'
+import './floating.css'
+
+import square from '../../assets/images/square.png'
+import triangle from '../../assets/images/triangle.png'
+import circle from '../../assets/images/circle.png'
 
 export default function Floating() {
   return (
     <>
-      <div className="shape square"></div>
-      <div className="shape triangle"></div>
-      <div className="shape circle"></div>
+      <div
+        className="shape square"
+        style={{ backgroundImage: `url(${square})` }}
+      ></div>
+
+      <div
+        className="shape triangle"
+        style={{ backgroundImage: `url(${triangle})` }}
+      ></div>
+
+      <div
+        className="shape circle"
+        style={{ backgroundImage: `url(${circle})` }}
+      ></div>
     </>
   )
 }
