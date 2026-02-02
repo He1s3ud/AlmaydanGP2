b826652b807348c4adbfca89beaa58c23f562e97
He1s3ud <131931996+He1s3ud@users.noreply.github.com>
2026-02-02 18:10:23 +0300

    try to display the logo

--- almaydan-frontend/src/components/Navbar.jsx
+++ almaydan-frontend/src/components/Navbar.jsx
@@ -1,35 +1,30 @@
 import React from "react";
-import './Navbar.css';
-import logo from '../assets/almaydanLogo.svg'
+import "./Navbar.css";
+import almaydanLogo from "../assets/almaydanLogo.svg";
 
+const Navbar = () => {
+  return (
+    <nav className="navbar">
 
-const Navbar = () =>{
-    return(
-        <nav className="navbar">
+      <div className="nav-logo">
+        <img src={almaydanLogo} alt="almaydan logo" />
+      </div>
 
-            <div className="nav-logo">
-                <img src="He1s3ud/AlmaydanGP2/almaydan-frontend/src/assets/almaydanLogo.svg" alt="almaydan logo"/>
-            </div> 
+      <ul className="nav-links">
+        <li>جدول المباريات</li>
+        <li><span className="live-dot">●</span> الآن</li>
+        <li>الساحة</li>
+        <li>المعسكر</li>
+        <li>اسال حميدان</li>
+        <li>حول</li>
+      </ul>
 
-            <ul className="nav-links">
-                <li>جدول المباريات </li>
-                <li><span className="live-dot">●</span>الآن</li>
-                <li>الساحة </li>
-                <li>المعسكر</li>
-                <li>اسال حميدان</li>
-                <li>حول</li>
+      <div className="nav-left-icons">
+        <span className="search-icon">🔍</span>
+      </div>
 
-            </ul>
+    </nav>
+  );
+};
 
-            <div className="nav-left-icons">
-                <span className="search-icon">🔍</span>
-            
-            </div>
-            
-
-
-        </nav>
-    )
-
-}
 export default Navbar;
