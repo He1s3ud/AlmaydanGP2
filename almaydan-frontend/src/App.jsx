218bdac3138c44828e30905073ca0c067c06d62f
He1s3ud <131931996+He1s3ud@users.noreply.github.com>
2026-02-02 14:02:11 +0300

    try to clean the code

--- almaydan-frontend/src/App.jsx
+++ almaydan-frontend/src/App.jsx
@@ -1,21 +1,18 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
+
 import Navbar from './components/Navbar'
+import Floating from './components/Floating/floating'
+import Hero from './sections/Hero/Hero'
+import Showcase from './components/showcase/showcase'
 
 function App() {
-
   return (
-    
     <>
-    <Navbar />
-    <floating />
-    <Hero />
-    <showcase  />
+      <Navbar />
+      <Floating />
+      <Hero />
+      <Showcase />
     </>
-  
-
   )
 }
 
