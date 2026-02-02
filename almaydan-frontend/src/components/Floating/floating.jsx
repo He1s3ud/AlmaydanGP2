3b8edf08a28f8532d05ec6ad79fcbe05e64b7b85
He1s3ud <131931996+He1s3ud@users.noreply.github.com>
2026-02-02 15:20:09 +0300

    tfdhdf

--- almaydan-frontend/src/components/Floating/floating.jsx
+++ almaydan-frontend/src/components/Floating/floating.jsx
@@ -6,21 +6,21 @@ import circle from '../../assets/images/circle.png'
 
 export default function Floating() {
   return (
-    <>
+    <div className="floating-container">
       <div
         className="shape square"
         style={{ backgroundImage: `url(${square})` }}
-      ></div>
+      />
 
       <div
         className="shape triangle"
         style={{ backgroundImage: `url(${triangle})` }}
-      ></div>
+      />
 
       <div
         className="shape circle"
         style={{ backgroundImage: `url(${circle})` }}
-      ></div>
-    </>
+      />
+    </div>
   )
 }
